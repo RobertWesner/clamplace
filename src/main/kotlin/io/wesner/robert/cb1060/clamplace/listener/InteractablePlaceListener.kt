83ffe544ec3b95b32a2399c2836ad766288d109e
RobertWesner <robert@wesner.io>
2026-03-05 22:17:00 +0100

    (actually ready now)

--- src/main/kotlin/io/wesner/robert/cb1060/clamplace/listener/InteractablePlaceListener.kt
+++ src/main/kotlin/io/wesner/robert/cb1060/clamplace/listener/InteractablePlaceListener.kt
@@ -11,6 +11,7 @@ import org.bukkit.block.BlockFace
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.material.Directional
@@ -77,6 +78,8 @@ class InteractablePlaceListener : Listener {
 
     @EventHandler(priority = Event.Priority.High, ignoreCancelled = true)
     fun onPlayerInteract(event: PlayerInteractEvent) {
+        if (event.action != Action.RIGHT_CLICK_BLOCK) return
+
         val (player, clicked, direction, target, item) = event.contextOrNull()
             ?: return
 
