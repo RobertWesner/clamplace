c177f40ba8dcf4f746c6ffec32b1fc68cac194de
RobertWesner <robert@wesner.io>
2026-03-08 04:19:37 +0100

    Slap unmergery magic, forealz!

--- src/main/kotlin/io/wesner/robert/cb1060/clamplace/listener/SlabDoNotStackListener.kt
+++ src/main/kotlin/io/wesner/robert/cb1060/clamplace/listener/SlabDoNotStackListener.kt
@@ -1,16 +1,11 @@
 package io.wesner.robert.cb1060.clamplace.listener
 
 import io.wesner.robert.cb1060.clamplace.BlockGroup
-import io.wesner.robert.cb1060.clamplace.ClamPlace
 import io.wesner.robert.cb1060.clamplace.asRevertible
 import io.wesner.robert.cb1060.clamplace.contextOrNull
-import io.wesner.robert.cb1060.clamplace.faceLookingAtBlock
-import io.wesner.robert.cb1060.clamplace.faceLookingAtBlockHorizontal
 import io.wesner.robert.cb1060.clamplace.isPlacementSuccessful
-import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.block.Block
-import org.bukkit.block.BlockFace
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -21,13 +16,13 @@ import org.bukkit.event.player.PlayerInteractEvent
 // side-clicking a block diagonally to the top of a slab should not just merge the slab
 
 class SlabDoNotStackListener : Listener {
+    val steps = setOf(Material.STEP, Material.DOUBLE_STEP)
+
     var ignored: Block? = null
     var toPreserve: List<Triple<Block, Material, Byte>> = listOf()
 
     @EventHandler(priority = Event.Priority.High, ignoreCancelled = true)
     fun onPlayerInteract(event: PlayerInteractEvent) {
-        Bukkit.getLogger().info { event.toString() }
-
         if (event.action != Action.RIGHT_CLICK_BLOCK) return
 
         val (player, clicked, direction, target, item, below) = event.contextOrNull()
@@ -36,8 +31,8 @@ class SlabDoNotStackListener : Listener {
         if (target.type !in BlockGroup.replaceable) return
 
         if (
-            item.type != Material.STEP
-            || below.type != Material.STEP
+            item.type !in steps
+            || below.type !in steps
             || item.type.data != below.type.data
         ) {
             return
@@ -51,7 +46,7 @@ class SlabDoNotStackListener : Listener {
 
                 Triple(block, block.type, block.data)
             }.filter {
-                it.first.type in setOf(Material.STEP, Material.DOUBLE_STEP)
+                it.first.type in steps
                 // DO NOT CHECK FOR DATA HERE (trust)
             }
         }
@@ -59,21 +54,33 @@ class SlabDoNotStackListener : Listener {
 
     @EventHandler(priority = Event.Priority.High, ignoreCancelled = true)
     fun onBlockPlace(event: BlockPlaceEvent) {
-        Bukkit.getLogger().info { event.toString() }
         if (event.block.x != ignored?.x || event.block.z != ignored?.z || ignored == null) return
 
         // has to be player! because event.itemInHand does not have data for some reason
         val item = event.player.itemInHand
-
-        // TODO: permission check via event
+        val target = ignored ?: return
+        val revert = target.asRevertible()
 
         // this needs to happen so there are no merges while setting them
         toPreserve.forEach { (block) -> block.type = Material.AIR }
 
-        ignored!!.setTypeIdAndData(item.type.id, item.data.data, true)
+        target.setTypeIdAndData(item.type.id, item.data.data, true)
         toPreserve.forEach { (block, type, data) -> block.setTypeIdAndData(type.id, data, true) }
 
         ignored = null
         toPreserve = listOf()
+
+        if (
+            !isPlacementSuccessful(
+                target,
+                target.state,
+                event.blockAgainst,
+                item,
+                event.player,
+            )
+        ) {
+            revert()
+            event.isCancelled = true
+        }
     }
 }
